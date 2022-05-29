da4c438132a85c751678749229d3c524d42f04ee
wtanabe09 <w.shoya.09@outlook.jp>
2022-05-29 16:18:35 +0900

    main_modelにリアルタイム取得のメソッドを追加

--- lib/main_model.dart
+++ lib/main_model.dart
@@ -2,14 +2,22 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class MainModel extends ChangeNotifier {
-  double lon = 0;
   double lat = 0;
-
+  double lon = 0;
   Future getPosition() async {
     final snapshot = FirebaseFirestore.instance.collection('place').doc('vEHSzDxi8X4FGiNm9tEY');
     final doc = await snapshot.get();
     this.lat = doc.get('lat');
     this.lon = doc.get('lon');
     notifyListeners();
+  }
+
+  void getPositionRealTime() {
+    final snapshots = FirebaseFirestore.instance.collection('place').doc('vEHSzDxi8X4FGiNm9tEY').snapshots();
+    snapshots.listen((snapshot) {
+      this.lat = snapshot.get('lat');
+      this.lon = snapshot.get('lon');
+      notifyListeners();
+    });
   }
 }
